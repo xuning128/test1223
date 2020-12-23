1424e2f047f0e0b36c713b3f7e7b3f5b61cc4a81
xuningaime2020 <75471477+xuning128@users.noreply.github.com>
2020-12-24 00:10:56 +0800

    try

---
+++ 1223test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1223test.MvcApplication" Language="C#" %>
---
+++ 1223test/ViewModels/CProductsVIewModel.cs
@@ -0,0 +1,24 @@
+﻿using _1223test.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace _1223test.ViewModels
+{
+    public class CProductsVIewModel
+    {
+        public Products entity_products { get; set; }
+
+        [DisplayName("商品序號")]
+        public int ID { get { return this.entity_products.ProductID; } }
+
+        [DisplayName("商品名稱")]
+        public string productname { get { return this.entity_products.ProductName; } }
+
+
+        public int? supplierID { get { return this.entity_products.SupplierID; } }
+
+    }
+}
